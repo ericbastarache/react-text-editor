95dd9a11c6399d7aa9191379e459942314a58aac
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-06 00:15:51 -0400

    Updated button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
+import "./Button.css";
 
 class Button extends Component {
   render() {
-    return <button />;
+    return (
+      <button onClick={this.props.onClick}>
+        <FontAwesome name={this.props.fontIcon} />
+        {this.props.buttonText}
+      </button>
+    );
   }
 }
+
+export default Button;
