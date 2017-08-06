2c426602d0abbb24aa39207a619e054785336bb4
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-06 00:16:26 -0400

    Updated editor container

--- src/containers/EditorContainer.js
+++ src/containers/EditorContainer.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
+import Editor from "../components/Editor/Editor";
+import Button from "../components/Button/Button";
+
 class EditorContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: "#000"
+    };
+  }
   render() {
-    return <div>Editor</div>;
+    return (
+      <div>
+        <div className="button-row">
+          <Button fontIcon="pencil" buttonText="Edit" />
+          <Button fontIcon="rocket" buttonText="Test" />
+        </div>
+        <div className="editor-area">
+          <Editor
+            rowCount="40"
+            colCount="300"
+            color={this.state.color}
+            handleChange={this.handleTextUpdate}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
